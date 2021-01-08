2e8dbdb8f2cdff60481da863f44e420d79c98835
Kazushi Isogai <kazu150@gmail.com>
2021-01-08 23:36:45 +0900

    エラー修正

--- components/todo.tsx
+++ components/todo.tsx
@@ -1,84 +1,87 @@
 import React, { useContext, FC, SyntheticEvent } from 'react';
-import { TodoContext } from '../pages'
+import { TodoContext } from '../pages';
 import { makeStyles } from '@material-ui/core/styles';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import TableBody from '@material-ui/core/TableBody';
 import Checkbox from '@material-ui/core/Checkbox';
-import { row } from '../pages/'
+import { row } from '../pages/';
 
 const Todo: FC = () => {
-    const {
-        setOpen,
-        state,
-        dispatch,
-        checked,
-        setChecked
-    } = useContext(TodoContext)
+    const { setOpen, state, dispatch, checked, setChecked } = useContext(
+        TodoContext
+    );
 
     const onItemClicked = (id: number) => {
-        setOpen(true)
+        setOpen(true);
         dispatch({
             type: 'selectedTodo_set',
-            payload: state.rows.filter((row: row) => row.id === id)[0]
-        })
-    }
+            payload: state.rows.filter((row: row) => row.id === id)[0],
+        });
+    };
 
-    const handleCheck = (event: SyntheticEvent) => {
-        setOpen(false)
-        dispatch({ type: 'selectedTodo_clear' })
-        
-        // TODO - エラー取れず
-        const isChecked: boolean = checked.length ? checked.some((item: string) => item === event.target.name) : false
-        if(isChecked){
-            setChecked([ ...checked ].filter((item: string) => item !== event.target.name))
+    const handleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setOpen(false);
+        dispatch({ type: 'selectedTodo_clear' });
+
+        const isChecked: boolean = checked.length
+            ? checked.some((item: string) => item === event.target.name)
+            : false;
+        if (isChecked) {
+            setChecked(
+                [...checked].filter(
+                    (item: string) => item !== event.target.name
+                )
+            );
         } else {
-            setChecked([ ...checked, event.target.name ])
+            setChecked([...checked, event.target.name]);
         }
     };
-    
 
     const useStyles = makeStyles((theme) => ({
         normal: {
-            backgroundColor: "#fff"
+            backgroundColor: '#fff',
         },
         inProgress: {
-            backgroundColor: "#DDD"
+            backgroundColor: '#DDD',
         },
         finished: {
-            backgroundColor: "#555"
-        }
-
+            backgroundColor: '#555',
+        },
     }));
 
     const classes = useStyles();
-    
+
     return (
         <TableBody>
             {state.rows.map((row) => (
-                <TableRow 
-                    className={
-                        (() => {
-                            switch(row.status){
-                                case "未着手": 
-                                    return classes.normal;
-                                case "途中":
-                                    return classes.inProgress;
-                                case "完了":
-                                    return classes.finished;
-                                default:
-                                    return classes.normal;
-                            }
-                        })()
-                    }
-                    key={row.id} 
+                <TableRow
+                    className={(() => {
+                        switch (row.status) {
+                            case '未着手':
+                                return classes.normal;
+                            case '途中':
+                                return classes.inProgress;
+                            case '完了':
+                                return classes.finished;
+                            default:
+                                return classes.normal;
+                        }
+                    })()}
+                    key={row.id}
                     onClick={() => onItemClicked(row.id)}
                 >
                     <TableCell>
                         <Checkbox
                             onChange={handleCheck}
                             name={row.id.toString()}
-                            checked={checked.length ? checked.some(item => item === row.id.toString()) : false}
+                            checked={
+                                checked.length
+                                    ? checked.some(
+                                          (item) => item === row.id.toString()
+                                      )
+                                    : false
+                            }
                             color="primary"
                         />
                     </TableCell>
@@ -92,7 +95,7 @@ const Todo: FC = () => {
                 </TableRow>
             ))}
         </TableBody>
-    )
-}
+    );
+};
 
 export default Todo;
